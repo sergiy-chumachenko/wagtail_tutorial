f74e4fb855c5115239f310d434d2026cd27e09d6
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-11-05 23:55:12 +0200

    [UPDATE] blog app added

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 from wagtail.search import index
 
@@ -10,15 +13,41 @@ from wagtail.search import index
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    @property
+    def blogs(self):
+        blogs = BlogPage.objects.live().descendant_of(self)
+        blogs = blogs.order_by('-date')
+        return blogs
+
     def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
-        context = super().get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
+        blogs = self.blogs
+        # Пагинация
+        page = request.GET.get('page')
+        paginator = Paginator(blogs, 9)
+        try:
+            blogs = paginator.page(page)
+        except PageNotAnInteger:
+            blogs = paginator.page(1)
+        except EmptyPage:
+            blogs = paginator.page(paginator.num_pages)
+
+        context = super(BlogIndexPage, self).get_context(request)
+        context['blogs'] = blogs
         return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
 
 
 class BlogPage(Page):
+    main_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
@@ -30,6 +59,7 @@ class BlogPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
+        ImageChooserPanel('main_image'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
     ]
