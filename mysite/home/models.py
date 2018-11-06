a9eda032cff52d2615b02131d33307ce74f93d1f
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-11-06 18:05:00 +0200

    [UPDATE] slider in progress

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,13 +1,76 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import (FieldPanel,
+                                                InlinePanel,
+                                                MultiFieldPanel,
+                                                PageChooserPanel)
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+
+from modelcluster.fields import ParentalKey
+from wagtail.snippets.models import register_snippet
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    disc_t = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.SearchField('disc_t'),
+    ]
 
     content_panels = Page.content_panels + [
+        InlinePanel('slider_items', label="Slider"),
         FieldPanel('body', classname="full"),
+        FieldPanel('disc_t', classname="full"),
     ]
+
+
+class LinkFields(models.Model):
+    link_external = models.URLField("URL", blank=True)
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    @property
+    def link(self):
+        if self.link_page:
+            return self.link_page.url
+        else:
+            return self.link_external
+
+    panels = [
+        FieldPanel('link_external'),
+        PageChooserPanel('link_page'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class SliderItem(LinkFields):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    panels = [
+        ImageChooserPanel('image')
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class HomePageSliderItem(Orderable, SliderItem):
+    page = ParentalKey('HomePage', related_name='slider_items')
---
+++ mysite/home/templatetags/home_tags.py
@@ -0,0 +1,49 @@
+from django import template
+from blog.models import BlogPage
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    return context['request'].site.root_page
+
+
+def has_menu_children(page):
+    return page.get_children().live().in_menu().exists()
+
+
+@register.inclusion_tag('home/tags/top_menu.html', takes_context=True)
+def top_menu(context, parent, calling_page=None):
+    menuitems = parent.get_children().live().in_menu()
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        'request': context['request'],
+    }
+
+
+@register.inclusion_tag('home/tags/top_menu_children.html', takes_context=True)
+def top_menu_children(context, parent):
+    menuitems_children = parent.get_children()
+    menuitems_children = menuitems_children.live().in_menu()
+    return {
+        'parent': parent,
+        'menuitems_children': menuitems_children,
+        'request': context['request'],
+    }
+
+
+@register.inclusion_tag('home/tags/blog_listing_homepage.html', takes_context=True)
+def blog_listing_homepage(context, count=3):
+    blogs = BlogPage.objects.live().order_by('-date')
+    return {
+        'blogs': blogs[:count].select_related('main_image'),
+        'request': context['request'],
+    }
+
